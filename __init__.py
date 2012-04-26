32209800254fb608bf28ca1692350f8935fc791a
esa <esa@mcserver.ikioma>
2012-04-26 09:19:45 +0300

    Screen-functionality to start() and stop() _is_running() -function _does_exist() -function template templates for _create_screen() and _send_command()

---
+++ __init__.py
@@ -0,0 +1,3 @@
+from manage import start as start
+from manage import stop  as stop
+
--- manage.py
+++ manage.py
@@ -1,10 +1,38 @@
-def start:
+from subprocess import call, Popen, PIPE
+
+def start(name):
+	startcmd="ls"
+	cmd="/usr/bin/screen -dmS " + name + " ; /usr/bin/screen -S " + name + ' -p0 -X stuff "' + startcmd + '"\n'
+
+	call(cmd,shell=True)
+
+def stop(name):
+	stopcommand=""
+	cmd="/usr/bin/screen -S " + name + ' -p0 -X stuff "' + stopcmd + '"\n'
+
+def status(name):
+	if _does_exist(name):
+	    if _is_running(name):
+		return 1
+	    else:
+		return 0
+	else:
+	    return -1
+
+
+
+def _does_exist(name):
 	pass
 
-def stop:
+def _is_running(name):
+	screens = Popen(['screen', '-list'],stdout=PIPE).communicate()[0]
+	if '.' + name + '\t(' in screens:
+	    return True
+	else:
+	    return False
+
+def _create_screen(name):
 	pass
 
-def status:
+def _send_cmd(name, cmd):
 	pass
-
-
